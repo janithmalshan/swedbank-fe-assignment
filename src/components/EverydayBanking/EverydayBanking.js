a16b9e21100bc22de6e936b54be8bcd174bd204d
Janith Malshan <janithmalshan@gmail.com>
2023-12-06 16:29:19 +0200

    Everyday banking content

--- src/components/EverydayBanking/EverydayBanking.js
+++ src/components/EverydayBanking/EverydayBanking.js
@@ -2,6 +2,7 @@ import {Tab, Tabs} from "../Ui/Tab/Tab";
 import {useTabs, withTabs} from "../Ui/Tab/TabContext";
 import PaymentTab from "./PaymentTab";
 import CalculatorTab from "./CalculatorTab";
+import Button from "../Ui/Button/Button";
 import './style.scss';
 
 const tabs = {
@@ -10,16 +11,34 @@ const tabs = {
 }
 
 const EverydayBanking = () => {
-    const { setCurrentTab } = useTabs();
+    const {setCurrentTab} = useTabs();
     return (
-        <Tabs defaultTab={tabs.payment} tabs={tabs} onTabSelect={(tab) => setCurrentTab(tab)} className="everyday-banking__tab-container">
-            <Tab id={tabs.payment}>
-                <PaymentTab />
-            </Tab>
-            <Tab id={tabs.calculator}>
-                <CalculatorTab />
-            </Tab>
-        </Tabs>
+        <>
+            <Tabs defaultTab={tabs.payment} tabs={tabs} onTabSelect={(tab) => setCurrentTab(tab)} className="everyday-banking__tab-container">
+                <Tab id={tabs.payment}>
+                    <PaymentTab/>
+                </Tab>
+                <Tab id={tabs.calculator}>
+                    <CalculatorTab/>
+                </Tab>
+            </Tabs>
+            <section className="ui-section">
+                <div className="everyday-banking__static-content ui-flex-row">
+                    <div className="hello-balloon">
+                        Hello world!
+                    </div>
+                    <div className="ui-flex-col ui-flex-grow">
+                        <h2>Welcome</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <div className="ui-flex-row ui-space-between ui-align-center">
+                            <a href="#" className="ui-link">Read more</a>
+                            <Button label={"Go"} variant={"primary"}/>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
